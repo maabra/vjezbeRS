55e36e470e06942a6d89312937f24e4f8bf04525
DanielVoric <voricdaniel@gmail.com>
2024-11-19 22:02:29 +0100

    2/4.3 - 4.5

--- Zadaca_2/zadatak_4.py
+++ Zadaca_2/zadatak_4.py
@@ -56,7 +56,83 @@ print(kalkulator.oduzimanje())
 print(kalkulator.mnozenje())
 print(kalkulator.dijeljenje())
 print(kalkulator.potenciranje())
-print(kalkulator.korijen())
+print(kalkulator.korijen(), "\n")
 
 
 #3
+
+studenti = [
+{"ime": "Ivan", "prezime": "Ivić", "godine": 19, "ocjene": [5, 4, 3, 5, 2]},
+{"ime": "Marko", "prezime": "Marković", "godine": 22, "ocjene": [3, 4, 5, 2, 3]},
+{"ime": "Ana", "prezime": "Anić", "godine": 21, "ocjene": [5, 5, 5, 5, 5]},
+{"ime": "Petra", "prezime": "Petrić", "godine": 13, "ocjene": [2, 3, 2, 4, 3]},
+{"ime": "Iva", "prezime": "Ivić", "godine": 17, "ocjene": [4, 4, 4, 3, 5]},
+{"ime": "Mate", "prezime": "Matić", "godine": 18, "ocjene": [5, 5, 5, 5, 5]}
+]
+
+
+class Student:
+    def __init__(self, ime, prezime, godine, ocjene):
+        self.ime = ime
+        self.prezime = prezime
+        self.godine = godine
+        self.ocjene = ocjene
+
+    def prosjek(self):
+        return sum(self.ocjene) / len(self.ocjene)
+
+
+studenti_objekti = [Student(x["ime"], x["prezime"], x["godine"], x["ocjene"]) for x in studenti]
+
+for s in studenti_objekti:
+    print(f"{s.ime} {s.prezime}, godine: {s.godine}, ocjene: {s.ocjene}")
+
+
+#4
+
+class Krug:
+    def __init__(self, r):
+        self.r = r
+    
+    def opseg(self):
+        return 2*self.r*3.14
+    
+    def povrsina(self):
+        return (self.r**2) * 3.14
+        
+krug = Krug(float(input("Upisi radijus: ")))
+print(krug.opseg())
+print(krug.povrsina())
+
+#5
+class Radnik:
+    def __init__(self, ime, pozicija, placa):
+        self.ime = ime
+        self.pozicija = pozicija
+        self.placa = placa
+
+    def work(self):
+        return f"Radim na poziciji {self.pozicija} sa placom {self.placa} "
+    
+radnik = Radnik("Marko", "varioc", 5000)    
+print(radnik.work())
+
+class Manager(Radnik):
+    def __init__(self,ime, pozicija, placa, department):
+        super().__init__(ime, pozicija, placa)
+        self.department = department
+
+    def work(self):
+        return f"Radim na poziciji {self.pozicija} u odjelu {self.department}"
+
+    def give_raise(self, radnik, povecanje):
+        radnik.placa = radnik.placa + povecanje
+
+
+manager = Manager("Franko", "menager", 8000, "strojarstvo")
+manager.give_raise(radnik, 2000)
+
+print(manager.work())
+print(radnik.work())
+
+
